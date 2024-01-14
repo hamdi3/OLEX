a02385c8f5dd439ccac92b231ebb29aeadb07ea2
louai khodary <louai.z.khodary@gmail.com>
2024-01-14 03:39:50 +0200

    add it prodcut provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Login, Error, Home, About, Register } from './pages';
+import { Login, Error, Home, About, Register, Products } from './pages';
 import { AuthProvider } from './context/AuthContext ';
+import { ProductProvider } from './context/ProductContext';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='*' element={<Error />} />
-        </Routes>
+        <ProductProvider>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<Error />} />
+            <Route path='/products' element={<Products />} />
+          </Routes>
+        </ProductProvider>
       </AuthProvider>
     </Router>
   );
