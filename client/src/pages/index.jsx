a0b5fd6f3ca296bae13de9fed1367a82d60711bc
louai khodary <louai.z.khodary@gmail.com>
2024-01-15 01:27:30 +0200

    add it Checkout page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -4,4 +4,6 @@ import Register from './RegisterPage';
 import Error from './ErrorPage';
 import About from './AboutPage';
 import Products from './ProductsPage';
-export { Login, Home, Register, Error, About, Products };
+import Checkout from './CheckoutPage';
+
+export { Login, Home, Register, Error, About, Products, Checkout };
