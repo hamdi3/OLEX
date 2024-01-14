e9571bc18387a66555cdebe5eee87e929791d04f
louai khodary <louai.z.khodary@gmail.com>
2024-01-15 01:26:47 +0200

    testing image render from the api

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -1,43 +1,41 @@
 import { useProduct } from '../context/ProductContext';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+
 const ProductsPage = () => {
   const { products } = useProduct();
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      {products.map((item) => {
-        const { id, name, image, description } = item;
-        console.log(image, 'ds');
-        return (
-          <div key={id}>
-            <CardMedia
-              sx={{ height: 140 }}
-              component='img'
-              src={image}
-              alt={name}
-            />
-
-            <CardContent>
-              <Typography gutterBottom variant='h5' component='div'>
-                {name}
-              </Typography>
-              <Typography variant='body2' color='text.secondary'>
-                {description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size='small'>Share</Button>
-              <Button size='small'>Learn More</Button>
-            </CardActions>
-          </div>
-        );
-      })}
-    </Card>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {products.map((item) => (
+        <Card key={item.id} style={{ maxWidth: 345, margin: 16 }}>
+          <CardMedia
+            component='img'
+            alt={item.name}
+            height='140'
+            src={item.image}
+            onError={(e) => {
+              console.error(`Error loading image for ${item.name}:`, e);
+            }}
+            onLoad={() => {
+              console.log(`Image loaded successfully for ${item.name}`);
+            }}
+          />
+          <CardContent>
+            <Typography variant='h6' component='div'>
+              {item.name}
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              {item.description}
+            </Typography>
+          </CardContent>
+          {/* Log the image URL just before rendering */}
+          {console.log('Image URL:', item.image)}
+        </Card>
+      ))}
+    </div>
   );
 };
 
