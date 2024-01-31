384139251a022a07e24d8af110c92f303a24ad07
Hamdi <Hamdi3khalil@gmail.com>
2024-01-31 11:45:02 +0100

    FIX
    Added seller field to serializer

--- server/products/serializers.py
+++ server/products/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'image']
+        fields = ['id', 'name', 'description', 'price', 'image', 'seller']
