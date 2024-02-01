b764c6459b7170d8af23a54c14fed5e2af9a19eb
Hamdi <Hamdi3khalil@gmail.com>
2024-02-01 13:05:45 +0100

    CHANGE
    Changed the url for media

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users_api/', include('users.urls')),
     path('products_api/', include('products.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
