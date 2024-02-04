7aa49ad9b2210dab68ad711a323d3f97a6f40af3
louai khodary <louai.z.khodary@gmail.com>
2024-02-04 19:49:47 +0200

    TESTING

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,8 +1,6 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
 import { BsPlus, BsEyeFill } from 'react-icons/bs';
-
 import { CartContext } from '../contexts/CartContext';
 
 const Product = ({ product }) => {
@@ -25,7 +23,7 @@ const Product = ({ product }) => {
         </div>
         {/* buttons */}
         <div className='absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
-          <button onClick={() => addToCart(product, id)}>
+          <button onClick={() => addToCart(id, product)}>
             <div className='flex justify-center items-center text-white w-12 h-12 bg-teal-500'>
               <BsPlus className='text-3xl' />
             </div>
