9165996f7a2235e4d7d4cb9c5d792c38d7f4b180
louai khodary <louai.z.khodary@gmail.com>
2024-02-04 19:47:33 +0200

    UPDATE
    implemented the color scheme in the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,7 @@
-import logo from '../assets/olex-logo.png';
 const Footer = () => {
   return (
-    <div className='flex items-end w-full  bg-white'>
-      <footer className='w-full text-gray-700 bg-gray-100'>
+    <div className='flex items-end w-full  bg-588157'>
+      <footer className='w-full text-F5E6E0 bg-588157'>
         <div className='container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap'>
           <div className='flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left'>
             <a className='flex items-center justify-center font-medium text-gray-900  md:justify-start'>
@@ -17,10 +16,10 @@ const Footer = () => {
                 />
               </svg>
             </a>
-            <p className='mt-2 text-sm text-gray-500'>Your ecommerce Home!</p>
+            <p className='mt-2 text-sm text-secondary'>Your ecommerce Home!</p>
             <div className='mt-4'>
               <span className='inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start'>
-                <a className='text-gray-500 cursor-pointer hover:text-gray-700'>
+                <a className='text-secondary cursor-pointer hover:text-gray-700'>
                   <svg
                     fill='currentColor'
                     strokeLinecap='round'
@@ -32,7 +31,7 @@ const Footer = () => {
                     <path d='M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z'></path>
                   </svg>
                 </a>
-                <a className='ml-3 text-gray-500 cursor-pointer hover:text-gray-700'>
+                <a className='ml-3 text-secondary cursor-pointer hover:text-gray-700'>
                   <svg
                     fill='currentColor'
                     strokeLinecap='round'
@@ -44,7 +43,7 @@ const Footer = () => {
                     <path d='M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z'></path>
                   </svg>
                 </a>
-                <a className='ml-3 text-gray-500 cursor-pointer hover:text-gray-700'>
+                <a className='ml-3 text-secondary cursor-pointer hover:text-gray-700'>
                   <svg
                     fill='none'
                     stroke='currentColor'
@@ -65,7 +64,7 @@ const Footer = () => {
                     <path d='M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01'></path>
                   </svg>
                 </a>
-                <a className='ml-3 text-gray-500 cursor-pointer hover:text-gray-700'>
+                <a className='ml-3 text-secondary cursor-pointer hover:text-gray-700'>
                   <svg
                     fill='currentColor'
                     stroke='currentColor'
@@ -87,83 +86,83 @@ const Footer = () => {
           </div>
           <div className='flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left'>
             <div className='w-full px-4 lg:w-1/4 md:w-1/2'>
-              <h2 className='mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase '>
+              <h2 className='mb-3 text-sm font-medium tracking-widest text-secondary uppercase '>
                 About
               </h2>
               <nav className='mb-10 list-none'>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Company
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Careers
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Blog
                   </a>
                 </li>
               </nav>
             </div>
             <div className='w-full px-4 lg:w-1/4 md:w-1/2'>
-              <h2 className='mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font'>
+              <h2 className='mb-3 text-sm font-medium tracking-widest text-secondary uppercase title-font'>
                 Support
               </h2>
               <nav className='mb-10 list-none'>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Contact Support
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Help Resources
                   </a>
                 </li>
               </nav>
             </div>
             <div className='w-full px-4 lg:w-1/4 md:w-1/2'>
-              <h2 className='mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font'>
+              <h2 className='mb-3 text-sm font-medium tracking-widest text-secondary uppercase title-font'>
                 Platform
               </h2>
               <nav className='mb-10 list-none'>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Terms &amp; Privacy
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Pricing
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     FAQ
                   </a>
                 </li>
               </nav>
             </div>
             <div className='w-full px-4 lg:w-1/4 md:w-1/2'>
-              <h2 className='mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font'>
+              <h2 className='mb-3 text-sm font-medium tracking-widest text-secondary uppercase title-font'>
                 Contact
               </h2>
               <nav className='mb-10 list-none'>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Send a Message
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     Request a Quote
                   </a>
                 </li>
                 <li className='mt-3'>
-                  <a className='text-gray-500 cursor-pointer hover:text-gray-900'>
+                  <a className='text-secondary cursor-pointer hover:text-gray-900'>
                     +123-456-7890
                   </a>
                 </li>
